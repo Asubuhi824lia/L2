4107e4f85d44dff47844b4ad91e4602c6d5e3037
Asubuhi824lia <der_anastasi84@mail.ru>
2023-11-27 20:54:50 +0700

    add: add new product

--- 9 Calorie Calc/src/js/main.js
+++ 9 Calorie Calc/src/js/main.js
@@ -1,5 +1,15 @@
+
+globalThis.prodList
 document.addEventListener('DOMContentLoaded',()=>{
-    document.getElementById('dayGoal').textContent = localStorage.getItem('CalorieCalc_goal')
+    const LSgoal = localStorage.getItem('CalorieCalc_goal')
+    document.getElementById('dayGoal').textContent = (!LSgoal||LSgoal=='') ? 'Укажите норму!' : LSgoal
+
+    // получить список из localStorage
+    prodList = JSON.parse(localStorage.getItem('CalorieCalc_prodList'))
+    if(!prodList) {
+        prodList = JSON.parse(JSON.stringify(prodListDef))
+    }
+    createProdList(prodList)
 })
 
 // Изменить целевые показатели калорийности на день
@@ -17,3 +27,98 @@ document.querySelector('#editDayGoal').addEventListener('click',()=>{
         localStorage.setItem('CalorieCalc_goal', `${goal} кКал`)
     }
 })
+
+
+const prodListDef = {
+    days: [
+        {
+            date:'26.11.2023',
+            products: [
+                {name: 'Банан', calories: '107', measure:'кКал.'},
+                {name: 'Леденцовая конфета', calories: '50', measure:'кал.'}
+            ]
+        }
+    ]
+}
+
+// Добавить продукт
+document.getElementById('addProdBtn').addEventListener('click',()=>{
+    const name = document.getElementById('getProduct').value.trim()
+    document.getElementById('getProduct').value =''
+    const calories = document.getElementById('getCalories').value.trim()
+    document.getElementById('getCalories').value=''
+    const measure = document.getElementById('measure').value
+
+    if(name=='' || calories=='') return;
+
+    const product = {name, calories, measure}
+    const curDate = getCurDate()
+
+    // сверяем текущую дату с последним днём
+    if(prodList.days.at(-1).date === curDate) {
+        prodList.days.at(-1).products.push(product)
+    } else {
+        prodList.days.push({date:curDate, products:[product]})
+    }
+
+    localStorage.setItem('CalorieCalc_prodList', JSON.stringify(prodList))
+    insertProd(product)
+})
+
+
+function getCurDate() {return (new Date).toLocaleDateString('ru', {day:'numeric', month:'numeric', year:'numeric'})}
+
+function insertProd(product) {
+    const lastDate = document.querySelectorAll('.day h3')[0].textContent
+
+    // установить соответствие даты
+    if(lastDate === getCurDate()) {
+        document.querySelectorAll('.day:first-child .product-card')[0]
+            .before(createProdNode(product))
+    } else {
+        document.querySelectorAll('.day')[0]
+            .before(createDayNode({date:getCurDate(), products:[product]}))
+    }
+}
+
+function createProdNode(product) {
+    const card = document.createElement('section')
+    card.classList.add('product-card')
+
+    const name = document.createElement('span')
+    name.classList.add('product-name')
+    name.textContent = product.name
+    
+    const calorie = document.createElement('span')
+    calorie.classList.add('product-calorie')
+    calorie.textContent = `${product.calories} ${product.measure}`
+
+    card.append(name, calorie)
+    return card
+}
+function createProdNodes(products) {
+    return products.map(product=>createProdNode(product)).reverse()
+}
+
+function createDayNode({date, products}) {
+    const day = document.createElement('section')
+    day.classList.add('day')
+    
+    const h = document.createElement('h3')
+    h.textContent = date
+    
+    const cards_list = document.createElement('section')
+    cards_list.classList.add('product-cards')
+    cards_list.append(...createProdNodes(products))
+    
+    day.append(h, cards_list)
+    return day
+}
+function createProdList(prodList) {
+    document.getElementById('productsList').innerHTML=''
+    
+    prodList.days.forEach((day, index) => {
+        if(index == 0) document.getElementById('productsList').appendChild(createDayNode(day))
+        else document.querySelector('#productsList .day').before(createDayNode(day))
+    });
+}
