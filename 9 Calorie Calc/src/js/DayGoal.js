08e39d33b3ea383560487ffe524598c0092af779
Asubuhi824lia <der_anastasi84@mail.ru>
2024-01-20 19:43:46 +0700

    add: checkDayLimit; fix: set daygoal

--- 9 Calorie Calc/src/js/DayGoal.js
+++ 9 Calorie Calc/src/js/DayGoal.js
@@ -2,19 +2,19 @@ import LS from './LS/LS.js'
 
 
 export default class DayGoal {
-    errMsgs = {
+    static errMsgs = {
         NAN: "Некорректное значение! Должны быть только цифры.",
         RANGE: "Некорректное значение! Число меньше сотни слишком мало.",
     }
     /**
      * Инициация запроса, обработка ввода и сохранение цели дня.
      */
-    constructor() {
-        this.goal = prompt("Введите целевой показатель калорийности на день (в кКал)")
+    static askDayGoal() {
+        const goal = prompt("Введите целевой показатель калорийности на день (в кКал)")
         
         if(goal == null || goal.length==0) return;
-        else if(isNaN(Number(goal)))alert(this.errMsgs.NAN);
-        else if(goal.length < 3)    alert(this.errMsgs.RANGE);
+        else if(isNaN(Number(goal)))alert(DayGoal.errMsgs.NAN);
+        else if(goal.length < 3)    alert(DayGoal.errMsgs.RANGE);
         else {
             document.getElementById('dayGoal').textContent = `${goal} кКал`
             LS.setGoal(goal)
@@ -26,6 +26,22 @@ export default class DayGoal {
     static set() {
         const LSgoal = LS.getGoal()
         document.getElementById('dayGoal')
-        .textContent = (!LSgoal || LSgoal=='') ? 'Укажите норму!' : LSgoal
+                .textContent = (!LSgoal || LSgoal=='') ? 'Укажите норму!' : LSgoal
+    }
+    static get() {
+        return LS.getGoal().split(' ')[0]
+    }
+
+    static checkDayLimit() {
+        let curDay = LS.getProducts().days.reverse()[0].products
+        curDay = curDay.map(prod => 
+            (prod.measure.length==5 ? Number(prod.calories)*1000 : prod.calories)
+        )
+        const curSum = curDay.reduce((count, calories) => count + Number(calories), 0)
+        const curDayGoal = Number(this.get())*1000
+
+        if(curSum > curDayGoal) {
+            alert("Дневной лимит калорий превышен!!")
+        }
     }
 }
